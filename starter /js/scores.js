a84941d2012fdccae67ad591d23ab07e629fa435
ms rashid <msrashidgb@gmail.com>
2022-12-12 18:52:49 +0000

    scoreboard and leaderboard function created in scores js file.

---
+++ starter /js/scores.js
@@ -0,0 +1,31 @@
+var scoreCard = document.querySelector('.wrapper')
+var leaderBoard = document.getElementById('highscores')
+
+
+function scoreBoard() {
+    removeAllChildNodes(leaderBoard);
+    scoreList = JSON.parse(localStorage.getItem("highScore"));
+    console.log(scoreList);
+    scoreList.sort((a, b) => {
+        return b.score - a.score;
+    });
+    //only render the top 4 scores.
+    topTen = scoreList.slice(0, 10);
+    addToLeaderBoard();
+}
+
+
+function addToLeaderBoard() {
+    scoreList = JSON.parse(localStorage.getItem("highScore"));
+    scoreList.forEach(element => {
+        scoreElement = document.createElement('div')
+        scoreElement.innerText = `${element.player} - ${element.score}`
+        leaderBoard.append(scoreElement)
+    });
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
